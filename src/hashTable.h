52e5c3808b18a25ad6d1db3bb78bab058f8d271b
defrell01 <sashka.sahkov@gmail.com>
2023-05-15 13:45:23 +0700

    ins/search/del methods

--- src/hashTable.h
+++ src/hashTable.h
@@ -1,13 +1,18 @@
 // структура айтемов хэш таблицы
-typedef struct {
-    char* key;
-    char* value;
+typedef struct
+{
+    char *key;
+    char *value;
 } ht_item;
 
 // структура самой хэш таблицы в которой хранится информация о размере и количество элементов
-typedef struct 
+typedef struct
 {
     int size;
     int count;
-    ht_item** items;
+    ht_item **items;
 } ht_hash_table;
+
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *ht, const char *key);
